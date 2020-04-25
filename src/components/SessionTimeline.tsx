995baaed49e601522c86df0c7c4cc7c23d80ec50
j- <j@skeoh.com>
2020-04-25 18:12:08 +1000

    Integrate new timeline component

--- src/components/SessionTimeline.tsx
+++ src/components/SessionTimeline.tsx
@@ -1,19 +1,15 @@
 import * as React from 'react';
 import { Session as SessionType } from '../types';
-import FormatTime from './FormatTime';
 import { useSelector } from 'react-redux';
 import { getAllFilms } from '../store';
-import { addTime, subtractTime } from '../time';
 import { useNow } from '../use-now';
-import './SessionTimeline.css';
+import Timeline from './Timeline';
 
 export interface Props {
   session: SessionType;
 }
 
-const DOORS_MINUTES = 10;
-const TRAILERS_MINUTES = 15;
-const DEFAULT_RUNTIME = 120;
+export const DEFAULT_RUNTIME = 120;
 
 const SessionTimeline: React.FC<Props> = ({ session }) => {
   const now = useNow();
@@ -22,28 +18,12 @@ const SessionTimeline: React.FC<Props> = ({ session }) => {
 
   if (!film) return null;
 
-  const { runtimeMinutes } = film;
-
-  const timeTrailers = session.time;
-  const timeDoors = subtractTime(timeTrailers, DOORS_MINUTES);
-  const timeStart = addTime(timeTrailers, TRAILERS_MINUTES);
-  const timeFinish = addTime(timeStart, runtimeMinutes || DEFAULT_RUNTIME);
-
   return (
-    <ul className="SessionTimeline">
-      <li className={now > timeDoors ? 'SessionTimeline-past' : 'SessionTimeline-future'}>
-        <FormatTime time={timeDoors} />: Doors open
-      </li>
-      <li className={now > timeTrailers ? 'SessionTimeline-past' : 'SessionTimeline-future'}>
-        <FormatTime time={timeTrailers} />: Trailers start ({DOORS_MINUTES} min. later)
-      </li>
-      <li className={now > timeStart ? 'SessionTimeline-past' : 'SessionTimeline-future'}>
-        <FormatTime time={timeStart} />: Movie starts ({TRAILERS_MINUTES} min. later)
-      </li>
-      <li className={now > timeFinish ? 'SessionTimeline-past' : 'SessionTimeline-future'}>
-        <FormatTime time={timeFinish} />: Movie ends ({runtimeMinutes} min. later)
-      </li>
-    </ul>
+    <Timeline
+      now={now}
+      timeTrailers={session.time}
+      runtimeMinutes={film.runtimeMinutes || DEFAULT_RUNTIME}
+    />
   );
 };
 
