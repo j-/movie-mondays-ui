d9b94df16a365030dff9f1d5977b5567013a194d
j- <j@skeoh.com>
2020-05-03 14:02:55 +1000

    Remove "sessions today" component

--- src/routes/SessionsTodayRoute.tsx
+++ src/routes/SessionsTodayRoute.tsx
@@ -3,18 +3,19 @@ import { RouteComponentProps } from 'react-router-dom';
 import parseDate from 'date-fns/parse';
 import Navigation from '../components/Navigation';
 import Week from '../components/Week';
-import SessionsToday from '../components/SessionsToday';
+import SessionsForDay from '../components/SessionsForDay';
 import { useToday } from '../use-today';
 
 const SessionsTodayRoute: React.FC<RouteComponentProps> = () => {
-  const today = parseDate(useToday(), 'yyyy-MM-dd', 0);
-  const selected = today;
+  const today = useToday()
+  const todayDate = parseDate(today, 'yyyy-MM-dd', 0);
+  const selectedDate = todayDate;
   return (
     <section className="SessionsTodayRoute">
       <h1 className="mb-5">Today's sessions</h1>
       <Navigation hideHomeLink={true} />
-      <Week today={today} selected={selected} />
-      <SessionsToday />
+      <Week today={todayDate} selected={selectedDate} />
+      <SessionsForDay date={today} />;
     </section>
   );
 };
